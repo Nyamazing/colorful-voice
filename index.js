f41b8c7f2c4244bbaf5e786beb7e6735ceaeadb8
Nyamazing <shukujitsu+nyamazing@gmail.com>
2015-04-01 15:42:56 +0900

    madamada

--- index.js
+++ index.js
@@ -44,22 +44,26 @@ module.exports = cv = function(obj){
 };
 
 cv.prototype.add = function(obj){
-  
+  var _inners = innersCopy(this._wrapped);
   if( obj == undefined){
     console.log('add as undefined');
-    return cv(this._wrapped);
+    return cv(_inners);
   } else if( obj instanceof cv ){
     console.log('add as cv');
-    return cv(this._wrapped.concat(obj._wrapped));
+    var _objInners = innersCopy(obj._wrapped);
+    return cv(_inners.concat(_objInners));
   } else if ( obj instanceof inner){
     console.log('add as inner');
-    return cv(this._wrapped.concat(obj));
+    var _inner = innerCopy(obj);
+    return cv(_inners.concat(_inner));
   } else if ( obj.length != undefined && obj[0] instanceof inner){
     console.log('add as inners');
-    return cv(this._wrapped.concat(obj));
+    var _objInners = innersCopy(obj);
+    return cv(_inners.concat(_objInners));
   } else {
     console.log('add as other');
-    return cv(this._wrapped[this._wrapped.length -1].values.concat(obj) );
+    _inners[_inners.length -1].values = _inners[_inners.length -1].values.concat(obj);
+    return cv( _inners );
   }
 };
 
@@ -75,7 +79,7 @@ var innerCopy = function(_inner){
   return i;
 }
 
-var wrappedCopy = function(wrapped){
+var innersCopy = function(wrapped){
   return wrapped.map(function(inner){
     return innerCopy(inner);
   });
@@ -84,9 +88,14 @@ var wrappedCopy = function(wrapped){
 cv.prototype._addColor = function(key,obj){
   var _color = colors[key];
   if( this._wrapped[this._wrapped.length -1].values.length === 0 ){
-    var n = wrappedCopy(this._wrapped);
+    var n = innersCopy(this._wrapped);
     n[n.length -1].color = _color;
     return cv( n );
+  } else {
+    var n = innersCopy(this._wrapped);
+    var _inner = new inner();
+    _inner.color = _color;
+    return cv( n.concat(_inner) );
   }
   // kokomade
   if( obj == undefined){
