0c6fd517c881f666deef761ee6d962379a5793e9
Nyamazing <shukujitsu+nyamazing@gmail.com>
2015-04-01 11:55:58 +0900

    change colors array to object

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var colors = [
-  ['black',   '\u001b[30m'],
-  ['red',     '\u001b[31m'],
-  ['green',   '\u001b[32m'],
-  ['yellow',  '\u001b[33m'],
-  ['blue',    '\u001b[34m'],
-  ['magenta', '\u001b[35m'],
-  ['cyan',    '\u001b[36m'],
-  ['white',   '\u001b[37m'],
-  ['reset',   '\u001b[0m' ],
-];
+var colors = {
+  black:   '\u001b[30m',
+  red:     '\u001b[31m',
+  green:   '\u001b[32m',
+  yellow:  '\u001b[33m',
+  blue:    '\u001b[34m',
+  magenta: '\u001b[35m',
+  cyan:    '\u001b[36m',
+  white:   '\u001b[37m',
+  reset:   '\u001b[0m' ,
+};
 
 module.exports = cv = function(obj){
   if (obj instanceof cv) return obj;
@@ -44,19 +44,20 @@ cv.prototype._addColor = function(_color,obj){
   }
 };
 
-colors.forEach(function(arr){
-  var color = arr[0];
-  var str   = arr[1];
-  cv.prototype[color] = function(obj){
+var keys = Object.keys(colors);
+
+keys.forEach(function(key){
+  var str   = colors[key];
+  cv.prototype[key] = function(obj){
     return this._addColor(str, obj);
   };
-  cv[color] = function(obj){
-    return cv()[color](obj);
+  cv[key] = function(obj){
+    return cv()[key](obj);
   };
 });
 
 cv.prototype.value = function() {
-  return this._wrapped.concat(colors[colors.length-1][1]);
+  return this._wrapped.concat(colors.reset);
 };
 
 cv.prototype.toString = function() {
