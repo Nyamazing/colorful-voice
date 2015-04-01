3e0a443713794f45ffbbd37418ca2854d0fc2673
Nyamazing <shukujitsu+nyamazing@gmail.com>
2015-04-01 14:23:22 +0900

    add inner object and fix constructor and add functions

--- index.js
+++ index.js
@@ -10,25 +10,56 @@ var colors = {
   reset:   '\u001b[0m' ,
 };
 
+var defaultColor = colors.white;
+
+var inner = function(obj){
+  if (obj instanceof inner) return obj;
+  if (!(this instanceof inner)) return new inner(obj);
+
+  this.color = defaultColor;
+  this.value = [];
+  if (obj != undefined) {
+    this.value = this.value.concat(obj);
+  }
+}
+
 module.exports = cv = function(obj){
+  console.log('new',obj);
   if (obj instanceof cv) return obj;
   if (!(this instanceof cv)) return new cv(obj);
 
   if (obj == undefined ){
-    this._wrapped = [];
+    console.log('new cv as undefined');
+    this._wrapped = [new inner()];
+  } else if (obj instanceof inner){
+    console.log('new cv as inner');
+    this._wrapped = [obj];
+  } else if (obj.length != undefined && obj[0] instanceof inner){
+    console.log('new cv as inners');
+    this._wrapped = obj;
   } else {
-    this._wrapped = ([]).concat(obj);
+    console.log('new cv as other');
+    this._wrapped = [new inner(obj)];
   }
 };
 
 cv.prototype.add = function(obj){
   
   if( obj == undefined){
+    console.log('add as undefined');
     return cv(this._wrapped);
   } else if( obj instanceof cv ){
+    console.log('add as cv');
     return cv(this._wrapped.concat(obj._wrapped));
-  } else {
+  } else if ( obj instanceof inner){
+    console.log('add as inner');
     return cv(this._wrapped.concat(obj));
+  } else if ( obj.length != undefined && obj[0] instanceof inner){
+    console.log('add as inners');
+    return cv(this._wrapped.concat(obj));
+  } else {
+    console.log('add as other');
+    return cv(this._wrapped[this._wrapped.length -1].value.concat(obj) );
   }
 };
 
